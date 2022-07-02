0006da3eb8f1d1fbc3c26baa82079e8503eed45b
Snag-hub <Snag-hub>
2022-07-02 21:03:28 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+    // Function to toggle the bar
+    function toggle(){
+        var x = document.getElementById("ToggleMenu");
+        if (x.style.display === "block") {
+            x.style.display = "none";
+        } else {
+            x.style.display = "block";
+        }
+    }
